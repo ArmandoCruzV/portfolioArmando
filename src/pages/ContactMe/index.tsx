2dc835e118041696c3eb9e726a172e112ee837b1
Haok <hao_2-d@hotmail.com>
2025-05-01 20:24:19 -0600

    Borrando la carpeta de iconos

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+//import React, { useEffect } from 'react';
 import { FaGithub, FaLinkedin, FaTiktok } from 'react-icons/fa';
 import { ScrollGSAP } from "../../AnimationsSroll";
+import "./style.css"
 
 const socialLinks = [
   {
@@ -20,21 +21,26 @@ const socialLinks = [
   },
 ];
 
-
 const ContactMe: React.FC = () => {
-
-  ScrollGSAP(".tiktok", 0, 20)
+  // useEffect(() => {
+  ScrollGSAP(".planet", 200, 0, "top 90%", "top 30%", 1);
+  //}, []);
 
   return (
     <div className="parallax">
-      <footer className="flex justify-center items-center p-4 text-white">
+      <div className="background">
+        <img src="https://img.freepik.com/free-psd/earth-ball-planet-isolated_23-2151806115.jpg?t=st=1746148283~exp=1746151883~hmac=6304d7dc1ea1828cc5562f69758fc1e358eaf5722174e1894e8ea920b14cde9e&w=740" className="planet2" alt="Planet 1" />
+        <img src="https://img.freepik.com/free-psd/earth-ball-planet-isolated_23-2151806115.jpg?t=st=1746148283~exp=1746151883~hmac=6304d7dc1ea1828cc5562f69758fc1e358eaf5722174e1894e8ea920b14cde9e&w=740" className="planet" alt="Planet 2" />
+        <img src="https://img.freepik.com/free-vector/astronaut_1308-83240.jpg?t=st=1746150838~exp=1746154438~hmac=3db6643559b11d22b8ad941ac84ddad445392fffbba1c981c09" className="astronaut" alt="Astronaut" />
+      </div>
+      <footer className="foter">
         {socialLinks.map((link) => (
           <a
             key={link.id}
             href={link.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="mx-4 text-2xl transition-colors duration-300 hover:text-blue-400"
+            className="link"
           >
             {link.icon}
           </a>
